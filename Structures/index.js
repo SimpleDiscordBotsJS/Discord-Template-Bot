e5e0a2225e0f386b35b4ba6cfa533fc686ac146d
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2022-05-03 20:52:26 +0300

    Structure improvement.

--- Structures/index.js
+++ Structures/index.js
@@ -1,6 +1,6 @@
 const { Client, Collection } = require("discord.js");
 const client = new Client({ intents: 32767 });
-const Logger = require("../Utilites/Logger");
+const { Warning, Error, Success } = require("../Utilites/Logger");
 const { BOT_TOKEN } = require("./config.json");
 const { promisify } = require("util");
 const { glob } = require("glob");
@@ -18,42 +18,59 @@ client.buttons = new Collection();
 
 //===========================================================
 
-// Анти-краш и прочее...
-process.on("unhandledRejection", (reason, p) => {
-    Logger.Warning('=== unhandled Rejection ==='.toUpperCase(),
+// Anti-Crash and more...
+process.on("unhandledRejection", (reason, p) => { Warning(
+    '=== unhandled Rejection ==='.toUpperCase(),
     'Reason: ' + reason.stack ? String(reason.stack) : String(reason),
     '==========================='.toUpperCase());
 });
-process.on("uncaughtException", (err, origin) => {
-    Logger.Error('=== uncaught Exception ==='.toUpperCase(),
+
+process.on("uncaughtException", (err, origin) => { Error(
+    '=== uncaught Exception ==='.toUpperCase(),
     'Exception: ' + err.stack ? err.stack : err,
     '==========================='.toUpperCase());
 });
-process.on('uncaughtExceptionMonitor', (err, origin) => {
-    Logger.Error('=== uncaught Exception Monitor ==='.toUpperCase());
+
+process.on('uncaughtExceptionMonitor', (err, origin) => { Error(
+    '=== uncaught Exception Monitor ==='.toUpperCase());
 });
-process.on('beforeExit', (code) => {
-    Logger.Warning('======= before Exit ======='.toUpperCase(),
+
+process.on('beforeExit', (code) => { Warning(
+    '======= before Exit ======='.toUpperCase(),
     'Code: ' + code,
     '==========================='.toUpperCase());
 });
-process.on('exit', (code) => {
-    Logger.Warning('========== exit =========='.toUpperCase(), 
+
+process.on('warning', (code) => { Warning(
+    '========= warning ========='.toUpperCase(),
+    'Code: ' + code,
+    '==========================='.toUpperCase());
+});
+
+process.on('exit', (code) => { Warning(
+    '========== exit =========='.toUpperCase(), 
     'Code: ' + code,
     '=========================='.toUpperCase());
 });
-process.on('multipleResolves', (type, promise, reason) => {
-    Logger.Warning('==== multiple Resolves ===='.toUpperCase(),
+
+process.on('multipleResolves', (type, promise, reason) => { Warning(
+    '==== multiple Resolves ===='.toUpperCase(),
     type, promise, reason,
     '==========================='.toUpperCase());
 });
 
 //===========================================================
 
-//Подключение к боту и вывод ошибки в случае отсутствия токена, или если токен неверен.
-client.login(BOT_TOKEN).catch(() => Logger.Error("[BOT] Invalid Bot Login Token."));
+
+client.login(BOT_TOKEN).catch(() => {
+    Error("[BOT] Invalid Bot Login Token.");
+    process.exit();
+});
 
 //===========================================================
 
-//Завершение работы
-process.on("SIGINT", () => { Logger.Success("SIGINT detected, exiting..."); process.exit(0); });
+
+process.on("SIGINT", () => { 
+    Success("SIGINT detected, exiting..."); 
+    process.exit(); 
+});
