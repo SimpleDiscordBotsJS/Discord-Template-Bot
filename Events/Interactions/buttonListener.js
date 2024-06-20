b1b6406aad37febe1fc4f2b26a0938cc2c321fa0
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2024-06-20 19:33:55 +0300

    [FEATURE] Add cooldown in a buttons

--- Events/Interactions/buttonListener.js
+++ Events/Interactions/buttonListener.js
@@ -1,4 +1,5 @@
 const { ButtonInteraction, Client } = require("discord.js");
+const cooldownUtil = require("../../Structures/Utilities/CooldownUtil");
 
 module.exports = {
     name: "interactionCreate",
@@ -10,20 +11,35 @@ module.exports = {
         if(!interaction.isButton()) return;
 
         const button = client.buttons.get(interaction.customId);
-        if(!button) return interaction.reply({ 
+        if(!button) return interaction.reply({
             content: "This button is outdated.", ephemeral: true
         });
 
         if(button == undefined) return;
 
-        if(button.permission && !interaction.member.permissions.has(button.permission)) return interaction.reply({ 
+        const cooldownTime = button.cooldown || 0
+        const userId = interaction.user.id
+
+        if(cooldownUtil.inCooldown(userId, button.customId)) {
+            const remainingTime = cooldownUtil.getCooldown(userId, interaction.customId);
+
+            return interaction.reply({
+                content: `Wait \`${remainingTime / 1000}\` seconds before using this button again.`, ephemeral: true
+            });
+        }
+
+        if(button.permission && !interaction.member.permissions.has(button.permission)) return interaction.reply({
             content: "You don't have the required permissions to use this.", ephemeral: true 
         });
 
-        if(button.developer && interaction.user.id !== client.config.DEV_ID) return interaction.reply({ 
+        if(button.developer && interaction.user.id !== client.config.DEV_ID) return interaction.reply({
             content: "This button is only available to the developer.", ephemeral: true
         });
 
         button.execute(interaction, client);
+
+        if(cooldownTime > 0) {
+            cooldownUtil.setCooldown(userId, button.customId, button.cooldown);
+        }
     },
 };
